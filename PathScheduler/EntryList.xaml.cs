2b4bf1d2c9f57bc22962c94b191399d96afdb07a
Jakub Gabała <jakub.gabala@gmail.com>
2019-11-02 16:27:10 +0100

    Added point view

--- PathScheduler/EntryList.xaml.cs
+++ PathScheduler/EntryList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PathScheduler.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +20,43 @@ namespace PathScheduler
     /// </summary>
     public partial class EntryList : Window
     {
+        private List<MapPoint> _pointList;
+
         public EntryList()
         {
             InitializeComponent();
+
+            _pointList = new List<MapPoint>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 30; i++)
+            {
+                _pointList.Add(new MapPoint
+                {
+                    Name = $"Punkt {i + 1}",
+                    CoordX = -100 + rand.Next(200),
+                    CoordY = -100 + rand.Next(200),
+                });
+            }
+
+            listViewEntries.ItemsSource = _pointList;
         }
 
         private void AcceptButton_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            _pointList.Add(new MapPoint
+            {
+                Name = "Punkt dodany ręcznie",
+                CoordX = 0,
+                CoordY = 0,
+            });
+            listViewEntries.Items.Refresh();
         }
     }
 }
